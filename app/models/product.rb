6f889341b05f28117c6f2030e88e56932e8b26de
zerolive <zerolive@gmail.com>
2015-09-29 12:02:42 +0200

    first push

---
+++ app/models/product.rb
@@ -0,0 +1,6 @@
+class Product < ActiveRecord::Base
+
+  validates :name,
+            :presence => true
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create(name: 'Apple', price: 1)
+Product.create(name: 'Orange', price: 1)
+Product.create(name: 'Pineapple', price: 2.4)
+Product.create(name: 'Marble cake', price: 3)
